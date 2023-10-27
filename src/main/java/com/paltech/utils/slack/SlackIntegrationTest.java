8150c17d47f850fa2ab2d3d7ae7950fa11cdb8f8
Isabella Huynh <isabella.huynh@pal.net.vn>
2023-10-27 13:21:21 +0700

    test gird selenium

--- src/main/java/com/paltech/utils/slack/SlackIntegrationTest.java
+++ src/main/java/com/paltech/utils/slack/SlackIntegrationTest.java
@@ -4,11 +4,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SlackIntegrationTest {
    public static void main(String[] args){
-       Eyes eyes = new Eyes();
-       eyes.setApiKey(System.getenv("API_KEY"));
-       ChromeDriver driver = new ChromeDriver();
-       TestResults res = eyes.close(false);
-       EyesSlack.post(res, System.getenv("SLACK_WEBHOOK_URL"));
+//       Eyes eyes = new Eyes();
+//       eyes.setApiKey(System.getenv("API_KEY"));
+//       ChromeDriver driver = new ChromeDriver();
+//       TestResults res = eyes.close(false);
+//       EyesSlack.post(res, System.getenv("SLACK_WEBHOOK_URL"));
    }
 }
 
