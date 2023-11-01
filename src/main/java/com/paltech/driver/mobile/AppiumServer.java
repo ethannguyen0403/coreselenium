bce8062b33136b771b4ea14eaf531705723c180e
Tim.Dang <tim.dang@pal.net.vn>
2023-11-01 10:43:07 +0700

    Upgrade appium version, modify appiumServer setting, update DesiredCapabilities to UIAutomator2 driver

--- src/main/java/com/paltech/driver/mobile/AppiumServer.java
+++ src/main/java/com/paltech/driver/mobile/AppiumServer.java
@@ -1,10 +1,8 @@
 package com.paltech.driver.mobile;
 
-import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
 import io.appium.java_client.service.local.flags.GeneralServerFlag;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.File;
 
@@ -12,20 +10,15 @@ public class AppiumServer {
     private static AppiumServiceBuilder builder;
     private static AppiumDriverLocalService service;
 
-    public static void startAppiumServer(DesiredCapabilities cap) {
+    public static void startAppiumServer() {
         builder = new AppiumServiceBuilder().withArgument(() -> "--base-path", "/wd/hub");
         builder.withArgument(() -> "--allow-insecure","chromedriver_autodownload");
         builder.withIPAddress("127.0.0.1");
-        builder.usingPort(4723);
-        builder.withCapabilities(cap);
-        builder.withArgument(GeneralServerFlag.SESSION_OVERRIDE, "true");
-        builder.withArgument(GeneralServerFlag.LOG_LEVEL,"error");
         builder.withLogFile(new File(System.getProperty("user.dir")+"appium.log"));
+        builder.usingPort(4723);
+        builder.withArgument(GeneralServerFlag.SESSION_OVERRIDE);
+        builder.withArgument(GeneralServerFlag.LOG_LEVEL,"error");
         service = AppiumDriverLocalService.buildService(builder);
-        service.start();
-        service.stop();
-        cap.setCapability("appium:chromeOptions", ImmutableMap.of("w3c", false));
-        builder.withCapabilities(cap);
         service.start();
     }
 
--- src/main/java/com/paltech/driver/mobile/MobileAndroidDriver.java
+++ src/main/java/com/paltech/driver/mobile/MobileAndroidDriver.java
@@ -1,11 +1,12 @@
 package com.paltech.driver.mobile;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
-import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.remote.AutomationName;
 import org.apache.commons.lang3.text.WordUtils;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import io.appium.java_client.remote.MobileCapabilityType;
 import com.paltech.driver.Driver;
 import com.paltech.driver.DriverProperties;
 import static com.paltech.driver.mobile.AppiumServer.startAppiumServer;
@@ -18,19 +19,20 @@ public class MobileAndroidDriver extends Driver {
 
 	public MobileAndroidDriver(DriverProperties properties) throws MalformedURLException {
 		super(properties);
-		DesiredCapabilities capabilities = new DesiredCapabilities();
-		capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, WordUtils.capitalize(properties.getPlatform().toString().toLowerCase()));
-		capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, properties.getPlatformVersion());
-		capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, properties.getDeviceName());
-		capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, WordUtils.capitalize(properties.getBrowserName().toString().toLowerCase()));
-		capabilities.setCapability(MobileCapabilityType.UDID, properties.getUDID());
-		capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 60);
-		capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, properties.getAutomationName());
-		capabilities.setCapability(MobileCapabilityType.NO_RESET, true);
-		startAppiumServer(capabilities);
-		setWebDriver(new AppiumDriver(new URL(properties.getRemoteURL()), capabilities));
+		UiAutomator2Options options = new UiAutomator2Options()
+				.setUdid(properties.getUDID())
+				.setDeviceName(properties.getDeviceName())
+				.setPlatformName(WordUtils.capitalize(properties.getPlatform().toString().toLowerCase()))
+				.setPlatformVersion(properties.getPlatformVersion())
+				.setNewCommandTimeout(Duration.ofSeconds(60))
+				.setAutomationName(AutomationName.ANDROID_UIAUTOMATOR2)
+				.withBrowserName(WordUtils.capitalize(properties.getBrowserName().toString().toLowerCase()))
+				.setSkipUnlock(true)
+				.setSkipDeviceInitialization(true)
+				.setNoReset(true);
+		startAppiumServer();
+		setWebDriver(new AndroidDriver(new URL(properties.getRemoteURL()), options));
 		getWebDriver().manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-//		setWebDriver(new AndroidDriver(new URL(properties.getRemoteURL()), capabilities));
 	}
 
 }
